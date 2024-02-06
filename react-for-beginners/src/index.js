e76a6a3ce81917bbeb43270389b4f4601305655d
naneunDustin <rlaejrqo465@naver.com>
2024-02-06 14:56:44 +0900

    7.7 Publishing

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 // import "./styles.css"; 여기에 import 하면 전체 적용
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+  root.render(
+  <App />
+  );
+</BrowserRouter>;
